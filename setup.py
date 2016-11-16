f696b5002dd6993908f80d6b999d29576fd00d4e
Alexander Maretskiy <amaretskiy@mirantis.com>
2016-11-16 19:18:37 +0200

    Create project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="availability",
+      version="0.1",
+      description="Collect cloud services availability data",
+      url="https://github.com/seecloud/availability",
+      author="<name>",
+      author_email="<name>@mirantis.com",
+      packages=["availability"])
